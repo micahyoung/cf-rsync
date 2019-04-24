2d84264e73f90599469a120269a23cea6d485a65
Micah Young <myoung@pivotal.io>
2019-04-23 20:44:51 -0400

    Move windows rsync deps under c:\Users\vcap\deps\cf-rsync

--- cf-rsh-windows.sh
+++ cf-rsh-windows.sh
@@ -7,4 +7,4 @@ rsync_app_name=$1
 rsync_args="$3 $4 $5 $6 $7"
 install_script_path="$(dirname $0)/remote-install-rsync-windows.ps1"
 encoded_command=$(iconv -f utf8 -t utf16le $install_script_path | base64 -w0 | tr -d '\n')
-exec cf ssh $rsync_app_name -c "IF EXIST deps\rsync.bat ( deps\rsync.bat $rsync_args ) ELSE ( powershell -EncodedCommand $encoded_command & deps\rsync.bat $rsync_args)"
+exec cf ssh $rsync_app_name -c "IF EXIST deps\cf-rsync\rsync.bat ( deps\cf-rsync\rsync.bat $rsync_args ) ELSE ( powershell -EncodedCommand $encoded_command & deps\cf-rsync\rsync.bat $rsync_args)"
--- remote-install-rsync-windows.ps1
+++ remote-install-rsync-windows.ps1
@@ -1,13 +1,14 @@
 #NO BOM
 $ProgressPreference="SilentlyContinue"
 
-$env:PATH="C:\Program Files\Git\usr\bin;C:\Program Files\Git\mingw64\bin;c:\Users\vcap\deps\usr\bin;$env:PATH"
+$env:PATH="C:\Program Files\Git\usr\bin;C:\Program Files\Git\mingw64\bin;c:\Users\vcap\deps\cf-rsync\usr\bin;$env:PATH"
 
-if (!(Test-Path "c:\Users\vcap\deps\usr\bin\rsync.exe")) {
-    Invoke-WebRequest -OutFile "c:\Users\vcap\rsync.pkg.tar.xz" 'http://repo.msys2.org/msys/x86_64/rsync2-3.1.3dev_msys2.7.0_r3-0-x86_64.pkg.tar.xz'
-    tar -x -C 'c:/Users/vcap/deps' -f "rsync.pkg.tar.xz" "usr/bin/rsync.exe" 
+if (!(Test-Path "c:\Users\vcap\deps\cf-rsync\usr\bin\rsync.exe")) {
+    New-Item -Type Directory "c:\Users\vcap\deps\cf-rsync"
+    Invoke-WebRequest -OutFile "rsync.pkg.tar.xz" 'http://repo.msys2.org/msys/x86_64/rsync2-3.1.3dev_msys2.7.0_r3-0-x86_64.pkg.tar.xz'
+    tar -x -C 'c:/Users/vcap/deps/cf-rsync' -f "rsync.pkg.tar.xz" "usr/bin/rsync.exe"
 }
 
 Write-Output '@echo off
-set PATH=C:\Program Files\Git\usr\bin;C:\Program Files\Git\mingw64\bin;c:\Users\vcap\deps\usr\bin;%PATH%
-rsync.exe %1 %2 %3 %4 %5 %6 %7' | Out-File -Encoding ASCII "c:\Users\vcap\deps\rsync.bat"
+set PATH=C:\Program Files\Git\usr\bin;C:\Program Files\Git\mingw64\bin;c:\Users\vcap\deps\cf-rsync\usr\bin;%PATH%
+rsync.exe %1 %2 %3 %4 %5 %6 %7' | Out-File -Encoding ASCII "c:\Users\vcap\deps\cf-rsync\rsync.bat"
