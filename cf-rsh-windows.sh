c27d29076421f14fca472996ce47b34c31707a1d
Micah Young <myoung@pivotal.io>
2019-04-23 20:10:09 -0400

    Change windows to allow usage outside rsh script directory

--- cf-rsh-windows.sh
+++ cf-rsh-windows.sh
@@ -5,6 +5,6 @@ set -o pipefail
 
 rsync_app_name=$1
 rsync_args="$3 $4 $5 $6 $7"
-
-encoded_command=$(iconv -f utf8 -t utf16le remote-install-rsync-windows.ps1 | base64 -w0 | tr -d '\n')
-exec cf ssh $rsync_app_name -c "IF EXIST deps\rsync.bat ( deps\rsync.bat $rsync_args ) else ( powershell -EncodedCommand $encoded_command & deps\rsync.bat $rsync_args)"
+install_script_path="$(dirname $0)/remote-install-rsync-windows.ps1"
+encoded_command=$(iconv -f utf8 -t utf16le $install_script_path | base64 -w0 | tr -d '\n')
+exec cf ssh $rsync_app_name -c "IF EXIST deps\rsync.bat ( deps\rsync.bat $rsync_args ) ELSE ( powershell -EncodedCommand $encoded_command & deps\rsync.bat $rsync_args)"
